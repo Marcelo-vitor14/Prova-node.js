04c26e442a56edae2dcd15bda2499ac149b2320d
Marcelo-vitor14 <mv.sarmento14@gmail.com>
2026-09-06 19:28:39 -0300

    Create UserMapper.js

---
+++ Mapper/UserMapper.js
@@ -0,0 +1,18 @@
+const UserDTO = require("../dtos/UserDTO");
+
+class UserMapper {
+  static toDTO(user) {
+    if (!user) return null;
+    return new UserDTO(user.id, user.name, user.email);
+  }
+
+  static toEntity(userDTOData) {
+    return {
+      name: userDTOData.name,
+      email: userDTOData.email,
+      password: userDTOData.password,
+    };
+  }
+}
+
+module.exports = UserMapper;
